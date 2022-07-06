1e23ec34e52d1da840c181ab040a15716382dcad
9963KK <62432023+9963KK@users.noreply.github.com>
2022-07-06 13:43:56 +0800

    7.6

--- Basic.h
+++ Basic.h
@@ -1,4 +1,23 @@
 #ifndef BASIC_H
 #define BASIC_H
-int get1();
+#include <stdio.h>
+#include "stdlib.h"
+typedef struct Elemtype
+{
+    int i;
+    char info[1024];
+} Elemtype;
+typedef struct LNode
+{
+    Elemtype data;
+    struct LNode *next;
+}LNode,*LinkList;
+
+
+typedef struct SqList
+{ 
+    Elemtype *data;
+    int length;
+} SqList;
+
 #endif
--- List/Linklist.cpp
+++ List/Linklist.cpp
@@ -1,7 +1,47 @@
 #include "stdio.h"
 #include "stdlib.h"
+#define MAXSIZE 100
+typedef struct Elemtype
+{
+    int i;
+    char info[1024];
+}Elemtype;
+
+typedef struct SqList
+{
+    Elemtype *data;
+    int length;
+}SqList;
+bool ListDelete(SqList &L,Elemtype e,int i){
+    if (i<1||i>L.length)
+    {
+        return false;
+    }
+    e = L.data[i - 1];
+    for (int j = i; j < L.length; j++)
+    {
+        L.data[j-1]=L.data[j];
+        L.length--;
+    }
+    return true;
+}
+int LocateElem(SqList L,Elemtype e){
+    int i=0;
+    for ( i = 0; i < L.length; i++)
+    {
+        if (e.i==L.data[i].i)
+        {
+            return i+1;
+        }
+        
+    }
+    return -1;
+    
+}
+//线性链表部分
+
 int main(){
 
 
-    return 1;
+   return 0;
 }
